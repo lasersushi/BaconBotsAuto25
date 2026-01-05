df7552186b4e6c8ac8f195e8461b28e854d396f0
lasersushi <lwalker30@priorypanther.com>
2026-01-05 09:09:25 -0800

    Adjusted strafeTicksToInches multiplier to be more accurate

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -40,7 +40,7 @@ public class Constants {
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
     public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
             .forwardTicksToInches(0.006656430078193553)
-            .strafeTicksToInches(0.027412195527067116)
+            .strafeTicksToInches(0.09409759998982468)
             .turnTicksToInches(0.012308574479837288)
             //.turnTicksToInches(0.006656430078193553)
             //.turnTicksToInches(0.013298574479837288)
