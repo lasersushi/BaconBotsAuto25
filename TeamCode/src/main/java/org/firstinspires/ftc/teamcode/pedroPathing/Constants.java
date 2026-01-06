ccb25e27f4d1d696affb419b0792819bbc0c135d
lasersushi <lwalker30@priorypanther.com>
2026-01-06 10:26:51 -0800

    More accurate constants for strafe velocity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -29,7 +29,7 @@ public class Constants {
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
             .xVelocity(75.21985835266673)
-            .yVelocity(26.760917660432078)
+            .yVelocity(32.00174190810328)
             .rightFrontMotorName("RightFront")
             .rightRearMotorName("RightBack")
             .leftRearMotorName("LeftBack")
