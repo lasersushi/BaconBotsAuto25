44cd0d04eebd3e0d63b9301e3063f8ed9ffa9822
lasersushi <lwalker30@priorypanther.com>
2026-01-11 11:30:54 -0800

    Adding shooter logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/PedroAutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/PedroAutoBlue.java
@@ -14,13 +14,13 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import org.opencv.core.Mat;
 
 import kotlin.math.UMathKt;
-//import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @Autonomous(name = "PedroAutoBlue", group = "Auto")
-public class PedroAutoBlue extends OpMode { 
-    //    public DcMotorEx ShooterMotor;
-//    public DcMotorEx IntakeMotor1;
-//    public DcMotorEx IntakeMotor2;
+public class PedroAutoBlue extends OpMode {
+    public DcMotorEx ShooterMotor;
+    public DcMotorEx IntakeMotor1;
+    public DcMotorEx IntakeMotor2;
     public void setPathState(int pState) {
         pathState = pState;
         pathTimer.resetTimer();
@@ -43,6 +43,7 @@ public class PedroAutoBlue extends OpMode {
         pathTimer = new Timer();
         opmodeTimer = new Timer();
         opmodeTimer.resetTimer();
+
 
         // Hardware Map
         //ShooterMotor = hardwareMap.get(DcMotorEx.class, "ShooterMotor");
@@ -177,222 +178,100 @@ public class PedroAutoBlue extends OpMode {
         switch (pathState) {
             case 0: {
                 follower.followPath(StartScore, true);
-//                ShooterMotor.setPower(1);
-//                IntakeMotor2.setPower(.5);
-//                try {
-//                    wait(150);
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }
-//                IntakeMotor2.setPower(0);
-//                try {
-//                    wait(150);
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }
-//                IntakeMotor2.setPower(1);
-//                try {
-//                    wait(150);
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }
-//                ShooterMotor.setPower(0);
-//                IntakeMotor2.setPower(0);
                 setPathState(1);
             }
             break;
 
             case 1:
                 if (!follower.isBusy()) {
-                    follower.followPath(GoPickup1);
+                    ShooterMotor.setPower(1);
+                    IntakeMotor2.setPower(.15);
                     setPathState(2);
                 }
                 break;
             case 2:
                 if (!follower.isBusy()) {
-                    follower.followPath(GrabPickup1);
-//                    ShooterMotor.setPower(-0.3);
-//                    IntakeMotor1.setPower(1);
-//                    try {
-//                        wait(300);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    IntakeMotor2.setPower(.5);
-//                    try {
-//                        wait(300);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    IntakeMotor1.setPower(0);
-//                    IntakeMotor2.setPower(0);
-//                    try {
-//                        wait(30);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    ShooterMotor.setPower(0);
+                    follower.followPath(GoPickup1);
+                    ShooterMotor.setPower(-.2);
+                    IntakeMotor2.setPower(0);
                     setPathState(3);
                 }
                 break;
             case 3:
                 if (!follower.isBusy()) {
-                    telemetry.addLine("It worked again!!!");
-                    follower.followPath(ScorePickup1, true);
-//                    ShooterMotor.setPower(1);
-//                    IntakeMotor2.setPower(.5);
-//                    try {
-//                        wait(150);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    IntakeMotor2.setPower(0);
-//                    try {
-//                        wait(150);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    IntakeMotor2.setPower(1);
-//                    try {
-//                        wait(150);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    ShooterMotor.setPower(0);
-//                    IntakeMotor2.setPower(0);
+                    follower.followPath(GrabPickup1);
+                    IntakeMotor1.setPower(1);
                     setPathState(4);
                 }
                 break;
-
             case 4:
                 if (!follower.isBusy()) {
-                    follower.followPath(GoPickup2);
-                    setPathState(5);
+                    telemetry.addLine("It worked again!!!");
+                    follower.followPath(ScorePickup1, true);
+                    IntakeMotor1.setPower(0);
+                    ShooterMotor.setPower(1);
+                    IntakeMotor2.setPower(.15);
+                    setPathState(4);
                 }
                 break;
 
             case 5:
                 if (!follower.isBusy()) {
-                    follower.followPath(GrabPickup2);
-//                    ShooterMotor.setPower(-0.3);
-//                    IntakeMotor1.setPower(1);
-//                    try {
-//                        wait(300);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    IntakeMotor2.setPower(.5);
-//                    try {
-//                        wait(300);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    IntakeMotor1.setPower(0);
-//                    IntakeMotor2.setPower(0);
-//                    try {
-//                        wait(30);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    ShooterMotor.setPower(0);
-                    setPathState(6);
+                    follower.followPath(GoPickup2);
+                    ShooterMotor.setPower(-.2);
+                    IntakeMotor1.setPower(1);
+                    setPathState(5);
                 }
                 break;
 
             case 6:
                 if (!follower.isBusy()) {
-                    follower.followPath(ScorePickup2, true);
-//                    ShooterMotor.setPower(1);
-//                    IntakeMotor2.setPower(.5);
-//                    try {
-//                        wait(150);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    IntakeMotor2.setPower(0);
-//                    try {
-//                        wait(150);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    IntakeMotor2.setPower(1);
-//                    try {
-//                        wait(150);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    ShooterMotor.setPower(0);
-//                    IntakeMotor2.setPower(0);
-                    setPathState(7);
+                    follower.followPath(GrabPickup2);
+                    setPathState(6);
                 }
                 break;
 
             case 7:
                 if (!follower.isBusy()) {
-                    follower.followPath(GoPickup3);
-                    setPathState(8);
+                    follower.followPath(ScorePickup2, true);
+                    IntakeMotor1.setPower(0);
+                    ShooterMotor.setPower(1);
+                    IntakeMotor2.setPower(.15);
+                    setPathState(7);
                 }
                 break;
 
             case 8:
                 if (!follower.isBusy()) {
-                    follower.followPath(GrabPickup3);
-//                    ShooterMotor.setPower(-0.3);
-//                    IntakeMotor1.setPower(1);
-//                    try {
-//                        wait(300);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    IntakeMotor2.setPower(.5);
-//                    try {
-//                        wait(300);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    IntakeMotor1.setPower(0);
-//                    IntakeMotor2.setPower(0);
-//                    try {
-//                        wait(30);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    ShooterMotor.setPower(0);
-                    setPathState(9);
+                    follower.followPath(GoPickup3);
+                    ShooterMotor.setPower(-.2);
+                    IntakeMotor1.setPower(1);
+                    setPathState(8);
                 }
                 break;
 
             case 9:
                 if (!follower.isBusy()) {
-                    follower.followPath(ScorePickup3, true);
-//                    ShooterMotor.setPower(1);
-//                    IntakeMotor2.setPower(.5);
-//                    try {
-//                        wait(150);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    IntakeMotor2.setPower(0);
-//                    try {
-//                        wait(150);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    IntakeMotor2.setPower(1);
-//                    try {
-//                        wait(150);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    ShooterMotor.setPower(0);
-//                    IntakeMotor2.setPower(0);
-                    setPathState(10);
+                    follower.followPath(GrabPickup3);
+                    setPathState(9);
                 }
                 break;
 
             case 10:
                 if (!follower.isBusy()) {
+                    follower.followPath(ScorePickup3, true);
+                    IntakeMotor1.setPower(0);
+                    ShooterMotor.setPower(1);
+                    setPathState(10);
+                }
+                break;
+
+            case 11:
+                if (!follower.isBusy()) {
                     follower.followPath(End);
+                    ShooterMotor.setPower(0);
+                    IntakeMotor1.setPower(0);
+                    IntakeMotor2.setPower(0);
                 }
                 break;
         }
